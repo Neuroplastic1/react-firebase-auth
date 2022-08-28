9a43b5e46cc0e6d19ff357442b09659d8f842f15
shafqat <64027495+Neuroplastic1@users.noreply.github.com>
2022-08-28 19:02:20 -0400

    Set up create user with firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Route, Routes} from 'react-router-dom'
+import Signin from './Auth-components/Signin'
+import Signup from './Auth-components/Signup'
+import Account from './Auth-components/Account'
+import { AuthContextProvider } from './Auth-components/AuthContext';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1 className='p-4'>setting up routes from app.js</h1>
+        <AuthContextProvider>
+        <Routes>
+          <Route path='/' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/account' element={<Account />} />
+        </Routes>
+        </AuthContextProvider>
     </div>
-  );
-}
+    )
+} 
 
 export default App;
