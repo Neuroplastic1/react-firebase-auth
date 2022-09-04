4aa5057c26fe6f8a4b97e4d97fbd3a740dcf42f2
shafqat <64027495+Neuroplastic1@users.noreply.github.com>
2022-09-03 22:51:42 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import Signin from "./Auth-components/Signin";
 import Signup from "./Auth-components/Signup";
 import Account from "./Auth-components/Account";
-import NavbarLeft from "./Components/Navbarleft";
+
 import { AuthContextProvider } from "./Auth-components/AuthContext";
 import AuthenticatedRoute from "./Auth-components/AuthenticatedRoute";
 const App = () => {
   return (
     <>
       <AuthContextProvider>
-        <NavbarLeft />
+        
         <Routes className="container">
           <Route path="/" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
